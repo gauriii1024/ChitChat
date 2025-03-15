7eda3c31b1fd4c04e854b2aeb59491d86e793243
gauriii1024 <gauri10022004@gmail.com>
2025-03-16 02:06:39 +0530

    Fix various frontend and backend files and add new components, pages, and store

---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore.js'
+
+
+const SettingsPage = () => {
+    const {authUser} = useAuthStore()
+  return (
+    <div>SettingsPage</div>
+  )
+}
+
+export default SettingsPage
